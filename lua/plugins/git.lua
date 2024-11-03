a350fe41c8e7276fa79a15e129ae8c11c3aa329c
vleo <velemer@pm.me>
2024-11-03 21:10:58 +0100

    Add gitgraph and undotree

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -12,4 +12,46 @@ return {
             { "<leader>G", ":Neogit<CR>", desc = "Neogit" },
         },
     },
+    {
+        "isakbm/gitgraph.nvim",
+        opts = {
+            symbols = {
+                merge_commit = "M",
+                commit = "*",
+            },
+            format = {
+                timestamp = "%H:%M:%S %d-%m-%Y",
+                fields = { "hash", "timestamp", "author", "branch_name", "tag" },
+            },
+            hooks = {
+                -- Check diff of a commit
+                on_select_commit = function(commit)
+                    vim.notify("DiffviewOpen " .. commit.hash .. "^!")
+                    vim.cmd(":DiffviewOpen " .. commit.hash .. "^!")
+                end,
+                -- Check diff from commit a -> commit b
+                on_select_range_commit = function(from, to)
+                    vim.notify("DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
+                    vim.cmd(":DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
+                end,
+            },
+        },
+        keys = {
+            {
+                "<leader>gl",
+                function()
+                    require("gitgraph").draw({}, { all = true, max_count = 5000 })
+                end,
+                desc = "GitGraph - Draw",
+            },
+        },
+    },
+    {
+        "jiaoshijie/undotree",
+        dependencies = "nvim-lua/plenary.nvim",
+        config = true,
+        keys = { -- load the plugin only when using it's keybinding:
+            { "<leader>gu", "<cmd>lua require('undotree').toggle()<cr>" },
+        },
+    },
 }
